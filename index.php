d2d8a98a1ca3cb91b95e95695a8088f17ce95f7b
AP_onishi <onishi@aprise.co.jp>
2025-03-26 09:44:29 +0900

    リンク変更コミット

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+
+	<title>昭和自動車グループ（静岡県浜松市） | お客様のカーライフをトータルサポート</title>
+	<meta name="description" content="車検整備、板金、リース、自動車保険と、自動車に関することなら何でもお任せください。静岡県西部の地元に密着して、お客様のカーライフをトータルサポートします。">
+	<meta name="keywords" content="昭和自動車,中古車,リース,浜松">
+
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/inc/head.php'); ?>
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/inc/ga.php'); ?>
+	
+	<link rel="stylesheet" href="/common/css/slick.css">
+</head>
+
+<body id="index">
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/inc/header.php'); ?>
+	
+	<div id="mainSlide">
+		<div class="wrap-common">
+			<div class="title"><img src="img/top/slide-title.png" alt="お客様のカーライフをトータルサポート"></div>
+		</div>
+		<div class="slide">
+			<div class="slideImg"><img src="img/top/slide-item01.png" alt=""></div>
+			<div class="slideImg"><img src="img/top/slide-item02.png" alt=""></div>
+			<div class="slideImg"><img src="img/top/slide-item03.png" alt=""></div>
+		</div>
+	</div>
+	
+	<div id="message" class="anchor">
+		<section class="bgGray topMessage">
+			<div class="wrap-common">
+				<h3 class="secTitle"><span class="main">MESSAGE</span>ごあいさつ</h3>
+				<div class="inner txtleft">
+					<h4 class="innerTitle">お客様の「ありがとう」を最大の喜びとして、<br class="tablet">お客様の視点に立つ最適なサービスをご提供します。</h4>
+					<p>
+						当社は自動車メンテナンスリース・輸入車ディーラー・メンテナンスに関連する商品の卸業など、トータルに自動車サービスの提供をしております。<br>
+						自動車のオーナー様によって、生活・趣味・営業車・運送車など、使用目的は様々です。私たち昭和自動車グループは多様化するお客様のお車に対するニーズに応えるため、常に変化する会社でありたいと日々努力しております。<br>
+						また、お客様のニーズに応えることを通じて、昭和自動車のスタッフ自身の成長にも繋げております。会社も個人も周囲の役に立てている事を自覚でき、生かされていることに素直に感謝のできる人材育成をする会社でもあります。<br>
+						<br>
+						「自動車をめぐる総合アドバイザー」として、お客様のお役に立てることは、私たちの最大の喜びです。その意味では、自動車をめぐるお客様の考え方やライフスタイルに丁寧に寄り添えることが、当グループならではの価値ともいえます。<br>
+						一人ひとりのお客様の身になって、痒いところに手が届くサービスをご提供できるよう、日々新たな想いを胸に、これからも全力で努力して参ります。
+					</p>
+					<p class="txtright sign">昭和自動車グループ 代表<span class="f-mincho">佐藤 辰美</span></p>
+				</div>
+			</div>		
+		</section>
+	</div>
+	
+	<div id="business" class="anchor">
+		<section class="topService">
+			<div class="wrap-common">
+				<h3 class="secTitle"><span class="main">SERVICE</span>事業案内</h3>
+				<div class="img mainImg">
+					<img src="img/top/img-service.png" alt="" class="tablet">
+					<img src="img/top/img-service-sp.png" alt="" class="sp">
+				</div>
+				<div class="inner">
+					<h4 class="innerTitle tablet">当グループは、自動車の所有に関わるお客様のご要望に<br class="tablet">ワンストップでお応えする事業体制をとっています。</h4>
+					<p class="tablet">
+						新車・中古車販売を皮切りに、自動車保険、車検・整備のご相談など、<br class="tablet">
+						各拠点が複数の機能を併せ持つことで、お客様のご要望に速やかに対応しています。<br class="tablet">
+						自動車のある便利な暮らしを快適にサポートし、地域のお客様に高い満足をご提供できるよう、<br class="tablet">
+						これからも当グループ独自のサービス品質を向上させていきます。
+					</p>
+				</div>
+				<div class="serviceList">
+					<div class="flex col2">
+						<div class="item bankin">
+							<a href="https://www.showa21.jp/bankin/" target="_blank" rel="noopener noreferrer">
+								<div class="zoomBox onImg">
+									<div class="img zoom"><img src="img/top/btn-bankin_bg.jpg" alt="アップル鈑金浜松"></div>
+								</div>
+								<p class="txtleft exTxt">迅速な事故処理対応から、お客様への修理内容の説明、修理着工までカバーしています。</p>
+							</a>
+						</div>
+						<div class="item syaken">
+							<a href="https://www.showa21.jp/apl/" target="_blank" rel="noopener noreferrer">
+								<div class="zoomBox onImg">
+									<div class="img zoom"><img src="img/top/btn-syaken_bg.jpg" alt="アップル車検浜松"></div>
+								</div>
+								<p class="txtleft exTxt">アップル車検をはじめ自動車のメンテナンスに関わる全てに対応しています。</p>
+							</a>
+						</div>
+					</div>
+					<div class="flex col3">
+						<div class="item">
+							<!--<a href="https://showa21.jp/mycar/" target="_blank" rel="noopener noreferrer">
+								<div class="zoomBox">
+									<div class="img zoom"><img src="img/top/bnr-img01.jpg" alt="BARDAHL"></div>
+								</div>
+								<p class="txtleft">
+									<span class="exTxt">マイカーリース</span>
+									オプションの追加で、車検・税金・整備・保険、すべてまとめて、定額で新車の乗れるリースです。
+								</p>
+								<div class="linkBox">
+									<div class="linkTxt icon clone"><span>サイトはこちら</span></div>
+								</div>
+							</a>-->
+								<div class="">
+									<div class="img"><img src="img/top/bnr-img01.jpg" alt="BARDAHL"></div>
+								</div>
+								<p class="txtleft">
+									<span class="exTxt">マイカーリース</span>
+									オプションの追加で、車検・税金・整備・保険、すべてまとめて、定額で新車の乗れるリースです。
+								</p>
+						</div>
+						<div class="item">
+							<div class="">
+								<div class="img"><img src="img/top/bnr-img02.jpg" alt="新車販売"></div>
+							</div>
+							<p class="txtleft">
+								<span class="exTxt">新車販売</span>
+								一人ひとりのお客様の身になって、痒いところに手が届くサービスをご提供できるよう、日々新たな想いを胸に、これからも全力で努力して参ります。
+							</p>
+						</div>
+						<div class="item">
+							<div class="">
+								<div class="img"><img src="img/top/bnr-img03.jpg" alt="中古車販売"></div>
+							</div>
+							<p class="txtleft">
+								<span class="exTxt">中古車販売</span>
+								お客様とゆっくりお話をさせていただきながら、アフターメンテナンスも視野に入れた最適な中古車をご提案します。
+							</p>
+						</div>
+						<div class="item">
+							<div class="">
+								<div class="img"><img src="img/top/bnr-img04.jpg" alt="自動車保険"></div>
+							</div>
+							<p class="txtleft">
+								<span class="exTxt">自動車保険</span>
+								お客様の運転の頻度や目的などをお聞きし、わかりやすいご説明で納得してご契約いただけるよう努めています。
+							</p>
+						</div>
+						<div class="item">
+							<a href="https://carleaseshowa.jp/" target="_blank" rel="noopener noreferrer">
+								<div class="zoomBox onImg">
+									<div class="img zoom"><img src="img/top/bnr-img05.jpg" alt="BARDAHL"></div>
+								</div>
+								<p class="txtleft">
+									<span class="exTxt">【法人】カーリース・レンタカー</span>
+									静岡県西部の個人事業主様や一般法人、個人のお客様を対象に、リースおよびレンタカー事業を行っています。
+								</p>
+								<div class="linkBox">
+									<div class="linkTxt icon clone"><span>サイトはこちら</span></div>
+								</div>
+							</a>
+						</div>
+						<div class="item">
+							<a href="https://store.shopping.yahoo.co.jp/carleaseshowa/?sc_i=shp_pc_watchlist_store_mdWatchlistStore_2" target="_blank" rel="noopener noreferrer">
+								<div class="zoomBox onImg">
+									<div class="img zoom"><img src="img/top/bnr-img06.jpg" alt="BARDAHL"></div>
+								</div>
+								<p class="txtleft">
+									<span class="exTxt">潤滑油・添加剤関連事業・通販事業</span>
+									アメリカ・シアトルに本社をおく、油脂メーカーバーダル社のケミカルを中心に自動車用・工業用の潤滑油・添加剤を販売。
+								</p>
+								<div class="linkBox">
+									<div class="linkTxt icon clone"><span>サイトはこちら</span></div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>		
+		</section>
+	</div>
+	
+	<section class="topBnr">
+		<div class="flex col2">
+			<div class="item corporate">
+				<a href="/corporate/">
+					<div class="onArea">
+						<div class="zoomBox">
+							<div class="img zoom"><img src="img/top/bnr-corporate.jpg" alt=""></div>
+						</div>
+						<div class="onItem">
+							<div class="img"><img src="img/top/icon-corporate@2x.png" alt=""></div>
+							<span class="ttl">会社概要</span>
+							<span>昭和自動車グループの会社概要や沿革などを<br>掲載しております。</span>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="item recruit">
+				<a href="https://recruit.fuerubo.com/showa-group/top" target="_blank" rel="noopener noreferrer">
+					<div class="onArea">
+						<div class="zoomBox right">
+							<div class="img zoom"><img src="img/top/bnr-recruit.jpg" alt=""></div>
+						</div>
+						<div class="onItem">
+							<div class="img"><img src="img/top/icon-recruit@2x.png" alt=""></div>
+							<span class="ttl">採用情報</span>
+							<span>私たちと一緒に働きませんか？</span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</section>
+	
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/common/inc/footer.php'); ?>
+	
+	
+	<!-- script -->
+	<script src="/common/js/slick.min.js"></script>
+	<script>
+		//slide
+		$('.slide').slick({
+			autoplay: true,
+			autoplaySpeed: 0,
+			speed: 5000,
+			cssEase: "linear",
+			slidesToShow: 2,
+			swipe: false,
+			arrows: false,
+			pauseOnFocus: false,
+			pauseOnHover: false,
+			responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+					}
+				}
+			]
+		});    
+    </script>
+</body>
+</html>

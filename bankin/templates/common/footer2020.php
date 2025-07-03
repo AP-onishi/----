60d13e74629a1b10092d4fffbdaa86d44ea9bcca
AP_onishi <onishi@aprise.co.jp>
2025-07-03 15:22:07 +0900

    板金修正前コミット

---
+++ bankin/templates/common/footer2020.php
@@ -0,0 +1,78 @@
+<?php require($_SERVER['DOCUMENT_ROOT'].'/mycar/wp/wp-load.php'); ?>
+<footer>
+    <div class="builder">
+        <div class="pagetop"><a href="#top"><span class="fa fa-chevron-circle-right red"></span>&nbsp;トップへ戻る</a></div>
+    </div>
+
+    <div id="footerLine">
+        <div class="builder clearfix">
+            <div id="hamazo">
+                <div class="title">
+                    <a href="https://applehamamatu.hamazo.tv" target="_blank">
+                        <img src="/mycar/images/hamazo.png" alt="">
+                    </a>
+                </div>
+<ul class="op justify tL sp">
+<?php echo_case_list(8)?>
+</ul>
+            </div>
+            <div id="footerDesc">
+                <img src="/bankin/images/logo.png">
+                <a href="tel:0120148706" class="telLink" onClick="ga('send', 'event', 'tel','フッター','');">0120-148-706</a>
+                <table border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <th scope="row">営業時間</th>
+                        <td>平日・土・祝　9：00-19：00　（18：00以降は受付のみ）
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">定休日</th>
+                        <td>日曜日 / 年末年始・ＧＷ・お盆</td>
+                    </tr>
+                </table>
+                <a href="/bankin/inquiry/" onClick="ga('send', 'event', 'お問い合わせ','フッターボタン','');"><img src="/bankin/images/footer_inqbtn.png" alt="予約はこちら"></a>
+                <ul class="mb20 clearfix">
+                    <li><a href="/bankin/case/"><span class="fa fa-caret-right blue"></span>アップル板金</a></li>
+                    <li><a href="/bankin/sitemap/"><span class="fa fa-caret-right blue"></span>サイトマップ</a></li>
+                    <li><a href="/bankin/flow/"><span class="fa fa-caret-right blue"></span>作業の流れ</a></li>
+                    <li><a href="/bankin/policy/"><span class="fa fa-caret-right blue"></span>プライバシーポリシー</a></li>
+                    <li><a href="/bankin/staff/"><span class="fa fa-caret-right blue"></span>店舗・スタッフ紹介</a></li>
+                    <li>
+                        <a href="/bankin/inquiry/" onClick="ga('send', 'event', 'お問い合わせ','フッター','');"><span class="fa fa-caret-right blue"></span>お問い合わせ</a>
+                    </li>
+                    <li>
+                        <a href="/bankin/estimate/" onClick="ga('send', 'event', 'お見積り','フッター','');"><span class="fa fa-caret-right blue"></span>御見積もり</a>
+                    </li>
+                    <li>
+                        <a href="/bankin/repair/" onClick="ga('send', 'event', '修理ご予約','フッター','');"><span class="fa fa-caret-right blue"></span>修理ご予約</a>
+                    </li>
+                    <li><a href="/bankin/qa/"><span class="fa fa-caret-right blue"></span>Q&amp;A</a></li>
+                    <li><a href="/bankin/link/"><span class="fa fa-caret-right blue"></span>リンク</a></li>
+                </ul>
+                <a href="http://showa21.jp/apl/" target="_blank" class="linkBtnA bgRed" onClick="ga('send', 'event', 'アップル車検サイト','フッター','');">車検のご相談はこちら</a>
+            </div>
+        </div>
+
+
+    </div>
+    <address>copyright ©2016 アップル車検 浜松店. ALL Rights Reserved.</address>
+</footer>
+<div id="topback"><span class="fa fa-chevron-circle-up fa-fw"></span></div>
+
+<!-- Pardot設定 2019.12.16 -->
+<script type="text/javascript">
+    piAId = '834223';
+    piCId = '3457';
+    piHostname = 'pi.pardot.com';
+
+    (function() {
+        function async_load(){
+            var s = document.createElement('script'); s.type = 'text/javascript';
+            s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+            var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+        }
+        if(window.attachEvent) { window.attachEvent('onload', async_load); }
+        else { window.addEventListener('load', async_load, false); }
+    })();
+</script>
+
